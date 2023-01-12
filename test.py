2e8ad476f17e5af09b876c89d61a056565ee03df
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-01-12 15:55:05 +0200

    DNA

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-def get_leaders(numbers: list) -> list:
-    numbers.append(0)
-    return [numbers[i] for i in range(len(numbers) - 1) if numbers[i] > sum(numbers[i + 1:])]
+def DNA_strand(dna):
+    return dna.translate(str.maketrans("ATCG", "TAGC"))
 
 
-
-print(get_leaders([16, 17, 4, 3, 5, 2]))
-print(get_leaders([1, 2, 3, 4, 0]))
+print(DNA_strand("AAAA"))
